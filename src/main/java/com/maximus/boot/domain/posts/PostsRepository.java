6e23893a7fce62edb9a3007c71cb11e9b99a88fb
kmchabh <k10510838>
2022-06-18 23:56:20 +0900

    Initial commit

---
+++ src/main/java/com/maximus/boot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.maximus.boot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<posts, Long>{
+}
---
+++ src/main/java/com/maximus/boot/domain/posts/posts.java
@@ -0,0 +1,38 @@
+package com.maximus.boot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+
+public class posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
